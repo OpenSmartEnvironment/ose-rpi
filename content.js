e8602315571f419afc3fbc3dde25a362e5e7c1af
Jan Snasel <snaselj@gmail.com>
2015-01-04 15:55:36 +0000

    Feature: Display Raspberry Pi camera images using entry data streaming between peers.

--- content.js
+++ content.js
@@ -23,9 +23,9 @@ exports = require('ose')
 // Public {{{1
 exports.addFiles = function() {
   this.addModule('lib/index');
-  this.addModule('lib/rpi/index');
   this.addModule('lib/camera/index');
   this.addModule('lib/camera/bb/detail');
+  this.addModule('lib/rpi/index');
   this.addModule('lib/rpi/bb/detail');
   this.addModule('lib/rpi/bb/listDetail');
 };
